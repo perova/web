dc24cd7d46592faa4f4b5b1260ca40fd065ec37a
Ritarita <rita.perova@gmail.comm>
2017-08-23 11:54:06 +0300

    neissaugo cookies

--- cookies/index.php
+++ cookies/index.php
@@ -1,29 +1,38 @@
-<?php
- setcookie("lankytojas","Jonas",time()+60*60*24 ,"/");
-  echo "<a href='zinau.php'>Ieik i kita</a>";
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="index.php" method="POST">
+      <div class="">
+        <input type="text" name="name" value="">
+      </div>
+      <div class="">
+        <input type="text" name="surname" value="">
+      </div>
+      <div class="">
+        <input type="submit" value="Send">
+      </div>
+      <div class="">
+        <?php
+          if (isset($_COOKIE["lankytojas"])){
+                echo "sausianukas issaugotas.";
+                  }
+          ?>
+      </div>
+    </form>
+  </body>
+
+</html>
+      <?php
 
 
+    if (isset($_POST["username"]) && isset($_POST["surname"]) ){
+    setcookie("lankytojas",$_POST["name"]." ".$_POST["surname"],time()+60*60*24 ,"/");# code...
+  }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- ?>
+    echo "<a href='zinau.php'>Ieik i kita</a>";
+?>
--- cookies/zinau.php
+++ cookies/zinau.php
@@ -1,3 +1,5 @@
+
+
 <?php
 if(isset($_COOKIE["lankytojas"])){
 echo $_COOKIE["lankytojas"];
